575862f316d7f18cf4541cd96be466f75475b7b8
Abhishek G Kellur <126042098+abhishekg-kellur@users.noreply.github.com>
2023-07-25 22:40:32 +0530

    Polymorphism

---
+++ Main.java
@@ -0,0 +1,26 @@
+package POLYMORPHISM;
+
+abstract class Vehicle {
+	abstract void drive();
+}
+
+class Car extends Vehicle {
+	void drive() {
+		System.out.println("Car max speed 200km");
+	}
+}
+
+class Motorcycle extends Vehicle {
+	void drive() {
+		System.out.println("Motorcyle max speed 160km");
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Vehicle c = new Car();
+		Vehicle m = new Motorcycle();
+		c.drive();
+		m.drive();
+	}
+}
