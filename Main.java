d5594d7f45c1a2f6cb2492f28aed960b3870626b
Abhishek G Kellur <126042098+abhishekg-kellur@users.noreply.github.com>
2023-07-25 22:42:17 +0530

    Polymorphism

--- Main.java
+++ Main.java
@@ -1,51 +1,26 @@
-package ABSTRACTION;
-import java.util.Scanner;
+package POLYMORPHISM;
 
-abstract class Printer {
-	abstract void configuration();
-	abstract void display();
+abstract class Vehicle {
+	abstract void drive();
 }
 
-class DotMatrix extends Printer {
-	void configuration() {
-		System.out.println("DotMatrix configuration");
-	}
-	void display() {
-		System.out.println("DotMatrix display");
+class Car extends Vehicle {
+	void drive() {
+		System.out.println("Car max speed 200km");
 	}
 }
 
-class LaserJet extends Printer {
-	void configuration() {
-		System.out.println("LaserJet configuration");
-	}
-	void display() {
-		System.out.println("LaserJet display");
+class Motorcycle extends Vehicle {
+	void drive() {
+		System.out.println("Motorcyle max speed 160km");
 	}
 }
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Printer pr;
-		System.out.println("1.DotMatrix \n2.LaserJet \n3.Exit");
-		while (true) {
-			int ch = sc.nextInt();
-			switch (ch) {
-			case 1: pr = new DotMatrix();
-					pr.configuration();
-					pr.display();
-					break;
-			case 2: pr = new LaserJet();
-					pr.configuration();
-					pr.display();
-					break;
-			case 3: System.out.println("Exit--");
-					sc.close();
-					return;
-			default: System.out.println("Invalid choice");
-					break;
-			}
-		}
+		Vehicle c = new Car();
+		Vehicle m = new Motorcycle();
+		c.drive();
+		m.drive();
 	}
 }
