b6017eaed5208ad8f646029198a37420bcd479f5
Abhishek G Kellur <126042098+abhishekg-kellur@users.noreply.github.com>
2023-07-25 22:40:57 +0530

    Abstraction

--- Main.java
+++ Main.java
@@ -1,26 +1,51 @@
-package POLYMORPHISM;
+package ABSTRACTION;
+import java.util.Scanner;
 
-abstract class Vehicle {
-	abstract void drive();
+abstract class Printer {
+	abstract void configuration();
+	abstract void display();
 }
 
-class Car extends Vehicle {
-	void drive() {
-		System.out.println("Car max speed 200km");
+class DotMatrix extends Printer {
+	void configuration() {
+		System.out.println("DotMatrix configuration");
+	}
+	void display() {
+		System.out.println("DotMatrix display");
 	}
 }
 
-class Motorcycle extends Vehicle {
-	void drive() {
-		System.out.println("Motorcyle max speed 160km");
+class LaserJet extends Printer {
+	void configuration() {
+		System.out.println("LaserJet configuration");
+	}
+	void display() {
+		System.out.println("LaserJet display");
 	}
 }
 
 public class Main {
 	public static void main(String[] args) {
-		Vehicle c = new Car();
-		Vehicle m = new Motorcycle();
-		c.drive();
-		m.drive();
+		Scanner sc = new Scanner(System.in);
+		Printer pr;
+		System.out.println("1.DotMatrix \n2.LaserJet \n3.Exit");
+		while (true) {
+			int ch = sc.nextInt();
+			switch (ch) {
+			case 1: pr = new DotMatrix();
+					pr.configuration();
+					pr.display();
+					break;
+			case 2: pr = new LaserJet();
+					pr.configuration();
+					pr.display();
+					break;
+			case 3: System.out.println("Exit--");
+					sc.close();
+					return;
+			default: System.out.println("Invalid choice");
+					break;
+			}
+		}
 	}
 }
