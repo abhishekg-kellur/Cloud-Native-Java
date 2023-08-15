8ead0ef6385c6e6cd001f0a099269b3a64a2bee1
Abhishek G Kellur <126042098+abhishekg-kellur@users.noreply.github.com>
2023-08-15 22:08:33 +0530

    5th program - Thread concept

---
+++ MainThread.java
@@ -0,0 +1,50 @@
+package prg5;
+
+class Thread1 extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread1: " + i);
+            try {
+                Thread.sleep(2000); 
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Thread1 Finished");
+    }
+}
+
+class Thread2 extends Thread {
+    public void run() {
+        for (int i = 10; i <= 15; i++) {
+            System.out.println("Thread2: " + i);
+            try {
+                Thread.sleep(3000); 
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Thread2 Finished");
+    }
+}
+
+public class MainThread {
+    public static void main(String[] args) {
+        System.out.println("Main thread started");
+
+        Thread1 thread1 = new Thread1();
+        Thread2 thread2 = new Thread2();
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Main thread finished");
+    }
+}
